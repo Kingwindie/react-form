4083b197b575bb2e0f298a8fcfdc03cfd97abd17
Kingwindie <kingwindie@gmail.com>
2022-06-26 07:03:15 -0500

    Initial commit

---
+++ src/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Input() {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  return (
+    <>
+      <TextField></TextField>
+    </>
+  );
+}
+
+export default Input;
---
+++ src/Parent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "./Form";
+import Home from "./Home"
+
+
+function Parent(){
+return(
+  <>
+<Home />
+<Form />
+  </>
+)
+
+}
+
+export default Parent;
